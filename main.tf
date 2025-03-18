43f3d43f6637d318dd608fa8423b5289c269aa4a
p1tonidze <p1t0nidze422@gmail.com>
2025-03-18 16:49:45 +0200

    Solution

--- main.tf
+++ main.tf
@@ -1,3 +1,40 @@
-# 1. Deploy an S3 storage bucket 
+# 1. Deploy an S3 storage bucket
+resource "aws_s3_bucket" "grafana_backups" {
+  bucket = "grafana-backups-p1tonidze"
 
-# 2. Confugure bucket policy to allow grafana iam role to use storage 
+  tags = {
+    Name = "grafana_backups"
+  }
+}
+
+# 2. Confugure bucket policy to allow grafana iam role to use storage
+resource "aws_s3_bucket_policy" "grafana_backups_policy" {
+  bucket = aws_s3_bucket.grafana_backups.id
+  policy = data.aws_iam_policy_document.grafana_backup_policy.json
+}
+
+data "aws_iam_policy_document" "grafana_backup_policy" {
+  statement {
+    sid       = "AllowListBucket"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.grafana_backups.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+  }
+
+  statement {
+    sid       = "AllowGetPutObjects"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["${aws_s3_bucket.grafana_backups.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,1 @@
-grafana_iam_role_arn = ""
+grafana_iam_role_arn = "arn:aws:iam::481665112149:role/grafana"
